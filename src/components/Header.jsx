72a457be2e0f0a3eaf45f68352a0e0408defb755
Sofia Darke <sofia.darke@hyperisland.se>
2021-05-03 17:27:55 +0200

    month corrected

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,17 +3,18 @@ import React from "react";
 const Header = () => {
 	let thisYear = new Date().getFullYear();
 	let thisDay = new Date().getDate();
-	let thisMonth = new Date().getDay();
+	let thisMonth = new Date().getMonth();
 	let thisHours = new Date().getHours();
 	let thisMinute = new Date().getMinutes();
 
 	let minutes = thisMinute < 10 ? "0" + thisMinute : thisMinute;
 	let hours = thisHours < 10 ? "0" + thisHours : thisHours;
+	let month = thisMonth + 1;
 
 	return (
 		<header>
 			<p className="top">
-				Stockholm, Sweden {hours}:{minutes} {thisDay}/{thisMonth}/{thisYear}
+				Stockholm, Sweden {hours}:{minutes} {thisDay}/{month}/{thisYear}
 			</p>
 		</header>
 	);
